712f272625d16b2ccc807437e4d67910ca032a52
ajaybarath <ajaybarath3@gmail.com>
2021-08-24 16:02:14 +0530

    ADDED: fibonacci number

--- src/com/bridgelabz/fibonacciNumber/FibonacciSeries.java
+++ src/com/bridgelabz/fibonacciNumber/FibonacciSeries.java
@@ -1,5 +1,31 @@
 package com.bridgelabz.fibonacciNumber;
 
+import java.util.Scanner;
+
 public class FibonacciSeries {
+	
+	public static void main(String args[]) {
+		
+		int fibonacciLimit = 0;
+		
+		Scanner s = new Scanner(System.in);
+		
+		fibonacciLimit = s.nextInt();
+		
+		int previousNumber = 1;
+		int currentNumber = 1;
+		
+		System.out.print(previousNumber + ", " + currentNumber);
+		
+		for (int i=0;i<fibonacciLimit;i++) {
+			int nextNumber = previousNumber + currentNumber;
+			
+			previousNumber = currentNumber;
+			currentNumber = nextNumber;
+			
+			System.out.print(", " + nextNumber);
+		}
+		
+	}
 
 }
--- src/com/bridgelabz/stopwatchStimulator/StopwatchStimulator.java
+++ src/com/bridgelabz/stopwatchStimulator/StopwatchStimulator.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.stopwatchStimulator;
 
 public class StopwatchStimulator {
+	
+	
 
 }
