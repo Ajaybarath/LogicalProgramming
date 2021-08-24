e3988759729b31618b5f2317097649ee1a2ea270
ajaybarath <ajaybarath3@gmail.com>
2021-08-24 16:06:33 +0530

    ADDED: prime number program

--- src/com/bridgelabz/primeNumber/PrimeNumber.java
+++ src/com/bridgelabz/primeNumber/PrimeNumber.java
@@ -1,5 +1,31 @@
 package com.bridgelabz.primeNumber;
 
+import java.util.Scanner;
+
 public class PrimeNumber {
+	
+	public static void main(String args[]) {
+
+		int number;
+		boolean isPrime = true;
+		
+		Scanner s = new Scanner(System.in);
+		
+		number = s.nextInt();
+		
+		for (int i=2;i<number/2;i++) {
+			if (number % i == 0) {
+				isPrime = false;
+			}
+		}
+		
+		if (isPrime) {
+			System.out.println("The given number is prime");
+		}
+		else {
+			System.out.println("The given number is not prime");
+		}
+		
+	}
 
 }
