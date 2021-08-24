25880bb1ac084bd97c11b0bad5be664fb90c15ac
ajaybarath <ajaybarath3@gmail.com>
2021-08-24 16:15:53 +0530

    ADDED: perfect number program

--- src/com/bridgelabz/perfectNumber/PerfectNumber.java
+++ src/com/bridgelabz/perfectNumber/PerfectNumber.java
@@ -1,5 +1,32 @@
 package com.bridgelabz.perfectNumber;
 
+import java.util.Scanner;
+
 public class PerfectNumber {
+	
+	public static void main(String args[]) {
+
+		int number;
+		
+		Scanner s = new Scanner(System.in);
+		
+		number = s.nextInt();
+		
+		int sum = 0;
+		
+		for (int i=1;i<number;i++) {
+			if (number % i == 0) {
+				sum += i;
+			}
+		}
+		
+		if (sum == number) {
+			System.out.println("The given number is perfect number");
+		}
+		else {
+			System.out.println("The given number is not perfect number");
+		}
+		
+	}
 
 }
